05a11b69c9c410c525dbf6ed7d69edcc374f0f03
PatricNurczyk <115892428+PatricNurczyk@users.noreply.github.com>
2025-03-05 21:33:38 -0500

    Created Initial Project

---
+++ pocketarcanum/src/main/java/com/pattyn/pocketarcanum/PocketarcanumApplication.java
@@ -0,0 +1,13 @@
+package com.pattyn.pocketarcanum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketarcanumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketarcanumApplication.class, args);
+	}
+
+}
---
+++ pocketarcanum/src/test/java/com/pattyn/pocketarcanum/PocketarcanumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pattyn.pocketarcanum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketarcanumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
